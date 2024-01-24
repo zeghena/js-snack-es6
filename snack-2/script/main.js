8d4c6dc7c4967fea42f9728d34b481e46eebc53a
davidemarchini <marchini73@gmail.com>
2024-01-24 18:36:22 +0100

    news..tutti gli studenti

--- snack-2/script/main.js
+++ snack-2/script/main.js
@@ -13,8 +13,13 @@
 
 const data = [
    {id:213, name: "Marco Della Rovere", grades: 78},
-   {id:213, name: "Marco Della Rovere", grades: 68}, 
-   {id:110, name: "Marco Della Rovere", grades: 78} 
+   {id:110, name: "Paola Cortellessa", grades: 96}, 
+   {id:250, name: "Andrea Mantegna ", grades: 48}, 
+   {id:145, name: "Gaia Borromini ", grades: 74},
+   {id:196, name: "Luigi Grimaldello", grades: 68}, 
+   {id:102, name: "Piero della Francesca", grades: 50}, 
+   {id:120, name: "Francesca da Polenta", grades: 84}
+   
 ]
 
 const uppercase = data.map((studente) => {
